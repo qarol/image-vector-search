c72f0b2ebaadb4f550870c6f3829da6e1c1125cb
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2025-02-26 18:52:24 +0100

    Cleanup specs

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
   let(:photo) { create(:photo) }
+  let(:photo_copy) { create(:photo) }
 
   describe '#similar' do
-    it 'returns similar photos' do
-      expect(photo.similar).to eq([])
+    xit 'returns similar photos' do
+      expect(photo.similar).to eq([photo_copy])
     end
   end
 
   describe '.by_description' do
-    it 'returns photos by description' do
-      expect(Photo.by_description('a photo')).to eq([])
+    xit 'returns photos by description' do
+      expect(Photo.by_description('a photo')).to match_array([photo, photo_copy])
     end
   end
 end
