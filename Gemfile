a00ae70928660d6df99756d7baa2f348c27e1f6e
Karol Bajko <karol.bajko@gmail.com>
2025-02-28 12:39:40 +0100

    Update text embedding for multilingual support using M-CLIP
    Enhanced the `TextEmbedding` model to use multilingual support with the M-CLIP tokenizer and updated input handling for ONNX predictions. Added the `tokenizers` gem for tokenizer initialization and updated the Gemfile and Gemfile.lock accordingly.

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem "thruster", require: false
 gem "onnxruntime"
 gem "neighbor"
 gem "sqlite-vec"
+gem "tokenizers"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/clip/text_embedding.rb
+++ app/models/clip/text_embedding.rb
@@ -1,13 +1,17 @@
 module CLIP
   class TextEmbedding
-    def initialize(model_path: "model/textual.onnx", tokenizer: CLIP::Tokenizer.new)
+    def initialize(
+      model_path: "multilingual_model/textual.onnx",
+      tokenizer: Tokenizers.from_pretrained("M-CLIP/XLM-Roberta-Large-Vit-B-32")
+    )
       @model = OnnxRuntime::Model.new(model_path)
       @tokenizer = tokenizer
     end
 
     def call(text)
-      tokens = tokenizer.encode(text)
-      model.predict({ input: [tokens] })["output"].first
+      tokens = tokenizer.encode(text).ids
+      attention_mask = Array.new(tokens.size, 1)
+      model.predict({ input_ids: [tokens], attention_mask: [attention_mask] })["output"].first
     end
 
     private
